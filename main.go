bc3074190a3d1c286fda803e5774135835b470bc
hao88 <52927295@qq.com>
2024-06-30 10:06:37 +0800

    更多的日志

--- main.go
+++ main.go
@@ -13,6 +13,14 @@ func main() {
 		config.CreateEmpty().Save()
 		panic("请完善配置config.yml")
 	}
-	fmt.Println("Timer interval", conf.Timer)
+	fmt.Println("-Config INFO")
+	fmt.Printf("--UserAgent:%s\n", conf.UserAgent)
+	fmt.Printf("--TokenId:%s\n", conf.TokenId)
+	fmt.Printf("--LoginToken:%s\n", conf.LoginToken)
+	fmt.Printf("--Domain:%s\n", conf.Domain)
+	fmt.Printf("--SubDomain:%s\n", conf.SubDomain)
+	fmt.Printf("--Timer:%s\n", conf.Timer)
+	fmt.Printf("--Support:%s\n", conf.Support)
+	fmt.Println("-Start Success")
 	timer.Start(conf)
 }
